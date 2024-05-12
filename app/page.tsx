60244956bd9a1e9e8867f198ca9aa4ba60cd6c93
Callum <callumreilly122@gmail.com>
2024-05-12 19:52:02 +1000

    basic outline done

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,18 @@ import { Typewriter } from 'nextjs-simple-typewriter';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-950 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4 lg:dark:bg-zinc-800/30">
         Callum
-        <Typewriter words={['s Portfolio', 'Folio']}/> 
+        <Typewriter words={[ 'Folio', '\'s Portfolio']}/> 
+        </p>
+      </div>
+      <div className="mb-80 rounded-md lg:max-w-6xl min-h-12 bg-zinc-950 w-full h-full ">
+        <h1 className="text-[50px] mt-8 ml-5">
+          Hi, I'm Callum
+        </h1>
+        <p className="mb-5 ml-5">
+          I'm an aspiring dev studying out of Sydney, Australia.
         </p>
       </div>
     </main>
