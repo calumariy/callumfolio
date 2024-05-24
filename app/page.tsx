bf23319677476b6ac75dc6045f406684137cbdaf
Callum <callumreilly122@gmail.com>
2024-05-24 20:26:12 +1000

    Changine metadata: Added title, icon etc.

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import { Typewriter } from 'nextjs-simple-typewriter';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-950 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4 lg:dark:bg-zinc-800/30">
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="w-full max-w-5xl items-center font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full border-b border-gray-300 bg-gradient-to-b from-zinc-950 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4 lg:dark:bg-zinc-800/30">
         Callum
         <Typewriter words={[ 'Folio', '\'s Portfolio']}/> 
         </p>
       </div>
-      <div className="mb-80 rounded-md lg:max-w-6xl min-h-12 bg-zinc-950 w-full h-full ">
+      <div className="border-gray-600 lg:border mb-80 rounded-lg lg:max-w-6xl min-h-12 bg-zinc-950 w-full h-full mt-20">
         <h1 className="text-[50px] mt-8 ml-5">
           Hi, I&apos;m Callum
         </h1>
