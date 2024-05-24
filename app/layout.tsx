f4841b036565c6b8043745b59c1230acc698c428
Callum <callumreilly122@gmail.com>
2024-05-24 21:33:53 +1000

    Added a basic framwork for pages to come in the future.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { titillium } from "./ui/fonts.ts";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={titillium.className}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Titillium_Web } from 'next/font/google';
+
+export const titillium = Titillium_Web({ subsets: ['latin'], weight: ['400'] });
